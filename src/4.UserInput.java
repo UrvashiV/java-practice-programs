9eb20e334d920480e4586790eaef2e240f664fbd
UrvashiV <urvashiv768@gmail.com>
2026-04-06 23:34:19 +0530

    Add Java source files

--- src/4.UserInput.java
+++ src/4.UserInput.java
@@ -1,5 +1,8 @@
 import java.util.Scanner;
 class UserInput {
+
+
+
     static void main(){
         Scanner input = new Scanner(System.in);
         System.out.println("Please enter your name: ");
---
+++ src/58.Factorial.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class Factorial {
+    static void main(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to the factorial calculator");
+        System.out.print("Please enter your number: ");
+        int num= input.nextInt();
+        long fact= factorialIterative(num);
+        System.out.println("Your factorial is "+ fact);
+        long fact1= factorialRecursive(num);
+        System.out.println("Your factorial is "+ fact1);
+
+    }
+    public static long factorialRecursive(int num){
+        if (num==1) return 1;
+        return num * factorialRecursive(num-1);
+    }
+
+    public static long factorialIterative(int num){
+        long result =1;
+        for (int i = 1; i<= num; i++){
+            result *= i;
+        }
+        return result;
+    }
+}
